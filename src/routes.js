9e309fa99fb9a818d4a2fe0192f92ce500dbb20c
Silvio Henrique <silviohfc@gmail.com>
2020-10-26 16:36:33 -0300

    Created first layout HTML

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get("/", (req, res) => {
-    return res.send("Ok!")
+    return res.render("layout.njk")
 })
-
 
 module.exports = routes
