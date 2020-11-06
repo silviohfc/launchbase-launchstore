4ea149de1c81166d778a5cf6b9e629b1d7077259
Silvio Henrique <silviohfc@gmail.com>
2020-11-05 22:54:32 -0300

    Created homepage

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
+
 const ProductController = require('./app/controllers/ProductController')
+const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/HomeController')
 
-routes.get("/", (req, res) => {
-    return res.render("layout.njk")
-})
+/* ---------------------------------- HOME ---------------------------------- */
+routes.get("/", HomeController.index)
 
+/* --------------------------------- SEARCH --------------------------------- */
+routes.get('/products/search', SearchController.index)
+
+/* -------------------------------- PRODUCTS -------------------------------- */
 routes.get("/products/create", ProductController.create)
 routes.get("/products/:id", ProductController.show)
 routes.get("/products/:id/edit", ProductController.edit)
@@ -14,7 +20,6 @@ routes.get("/products/:id/edit", ProductController.edit)
 routes.post("/products", multer.array("photos", 6), ProductController.post)
 routes.put("/products", multer.array("photos", 6), ProductController.put)
 routes.delete("/products", ProductController.delete)
-
 
 /* ---------------------------------- ALIAS --------------------------------- */
 routes.get("/ads/create", (req, res) => {
